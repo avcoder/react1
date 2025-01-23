b22c4d5a32685b8b6c3411d8b22c39643391ebae
avcoder <avcoder@users.noreply.github.com>
2025-01-22 23:20:42 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,41 +17,24 @@ import {
 } from "@chakra-ui/react";
 
 function App() {
-  console.log(1);
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    console.log(2);
-    getData();
-  }, []);
-
-  console.log(3);
-  async function getData() {
-    console.log(4);
-    const res = await fetch("https://randomuser.me/api/?results=5");
-    const data = await res.json();
-    setData(data.results);
-  }
-
-  console.log(5);
   return (
     <>
       <Center>
         <SimpleGrid columns={1} spacing={4} width={700}>
-          {data.map((person) => (
-            <Card maxW="md">
+            <Card maxW="md" key={1}>
               <CardHeader>
                 <Flex>
                   <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
                     <Avatar
-                      name={`${person.name.first} ${person.name.last}`}
-                      src={person.picture.thumbnail}
+                      name='firstName lastName'
+                      src='image'
                     />
                     <Box>
                       <Heading
                         size="sm"
                         textAlign={"left"}
-                      >{`${person.name.first} ${person.name.last}`}</Heading>
-                      <Text>{`${person.location.city}, ${person.location.country}`}</Text>
+                      >{`firstName lastName`}</Heading>
+                      <Text>{`company title`}</Text>
                     </Box>
                   </Flex>
                   <Button colorScheme="blue" variant="outline">
@@ -60,8 +43,10 @@ function App() {
                 </Flex>
               </CardHeader>
             </Card>
-          ))}
         </SimpleGrid>
+        <Button colorScheme="green" onClick={() => {}}>
+          Add 1 more
+        </Button>
       </Center>
     </>
   );
